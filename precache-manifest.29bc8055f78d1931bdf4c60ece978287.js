b70b1d27647e6a02158cfb5bacc4916ea3ee6279
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-06-01 22:18:22 +0400

    Updates

---
+++ precache-manifest.29bc8055f78d1931bdf4c60ece978287.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4296385dd53e2b7496abf49e844973a",
+    "url": "/Wisemen/index.html"
+  },
+  {
+    "revision": "75d54404c0834e9373a7",
+    "url": "/Wisemen/static/css/main.07a2a1dc.chunk.css"
+  },
+  {
+    "revision": "a9e51c58bda7e5815991",
+    "url": "/Wisemen/static/js/2.6820d65f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Wisemen/static/js/2.6820d65f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d54404c0834e9373a7",
+    "url": "/Wisemen/static/js/main.1a0f2e56.chunk.js"
+  },
+  {
+    "revision": "b35bb9fe0512c3ecb80a",
+    "url": "/Wisemen/static/js/runtime-main.542802e3.js"
+  },
+  {
+    "revision": "19930786992ef91c583fc1a43f5e0b67",
+    "url": "/Wisemen/static/media/Wisemen-shadow2.19930786.png"
+  },
+  {
+    "revision": "ce0590f25784fe7cf6f52f910d28207d",
+    "url": "/Wisemen/static/media/about-shadow2.ce0590f2.png"
+  },
+  {
+    "revision": "72a9f6084e2093692cbdc6c18e824813",
+    "url": "/Wisemen/static/media/cтена-218.72a9f608.png"
+  },
+  {
+    "revision": "c13866850fe5345ae55a19e3c31fae88",
+    "url": "/Wisemen/static/media/header-bg-cloud80.c1386685.png"
+  },
+  {
+    "revision": "6ca8ebbf89012922ac41f5b5d59b30c7",
+    "url": "/Wisemen/static/media/intro-compress2.6ca8ebbf.mp4"
+  },
+  {
+    "revision": "ae8eb5b91a70f0e351cae30a01b5d3f3",
+    "url": "/Wisemen/static/media/login-shadow.ae8eb5b9.png"
+  },
+  {
+    "revision": "05dde4ffe15164df6ec4ddf6fff78f2b",
+    "url": "/Wisemen/static/media/main-bg-video-20.05dde4ff.mp4"
+  },
+  {
+    "revision": "01fb4adfa89a7db626d57bdac2a43b36",
+    "url": "/Wisemen/static/media/main-bg.01fb4adf.png"
+  },
+  {
+    "revision": "cb23fb2fd0e7b4115b78dceda0565116",
+    "url": "/Wisemen/static/media/point-shadow2.cb23fb2f.png"
+  },
+  {
+    "revision": "25920e393cdab2543b9dfede96712b96",
+    "url": "/Wisemen/static/media/sound-of.25920e39.svg"
+  },
+  {
+    "revision": "8ef34d2655298c572f5fa461a20a5e6d",
+    "url": "/Wisemen/static/media/sound-on.8ef34d26.svg"
+  },
+  {
+    "revision": "24dd3426abe6746b924be4e4894f2db1",
+    "url": "/Wisemen/static/media/taverna-shadow2.24dd3426.png"
+  }
+]);
