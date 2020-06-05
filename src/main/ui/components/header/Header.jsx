9bd06a5903f1cbe933786aec6013e5330ebfd7c9
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-06-05 16:18:04 +0400

    add: skip intro, close form-content

--- src/main/ui/components/header/Header.jsx
+++ src/main/ui/components/header/Header.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import styles from './Header.module.css'
-import imgLogin from '../../images/login-shadow.png'
+import castle from '../../icons/castle-40.png'
 import imgLogo from '../../images/Wisemen-shadow2.png'
 import Navbar from "../navbar/Navbar";
 
-const Header = ({ setModal }) => {
-
-	const activationModal = () => {
-		setModal (true);
-	};
-
+const Header = ({ setModal, setBg, toggleBg }) => {
+	const switchBg = () => {
+		setBg(!toggleBg);
+	}
 	return (
 		<div className={styles.header}>
 			<div className={styles.header__wrap}>
-				<div className={styles.header__logo}>
+				<div className={styles.header__logo} onClick={switchBg}>
 					<img src={imgLogo} alt="logo"/>
 				</div>
-				<Navbar/>
-				<div className={styles.header__login} id='login' onClick={activationModal}>
-					<img src={imgLogin} alt="login"/>
+				<Navbar setModal={setModal}/>
+				<div className={styles.header__home} id='login' >
+					<img src={castle} alt="login"/>
 				</div>
 			</div>
 		</div>
