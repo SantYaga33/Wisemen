d2bed662bb99713cd4a7dd9dfe7bef26ecccfcc7
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-06-26 18:49:53 +0400

    added game-block + decks +css + animation

--- src/main/ui/components/game/Game.jsx
+++ src/main/ui/components/game/Game.jsx
@@ -4,7 +4,9 @@ import Deck_blue from "./deck/Deck_blue";
 import Deck_red from "./deck/Deck_red";
 import Deck_green from "./deck/Deck_green";
 import Deck_yellow from "./deck/Deck_yellow";
-
+import ava from '../../images/shirt6.png';
+import Deck_green2 from "./deck/Deck_green2";
+import Deck_blue2 from "./deck/Deck_blue2";
 
 
 const Game = () => {
@@ -15,14 +17,67 @@ const Game = () => {
 			<div className={styles.game__container}>
 				<div className={styles.game__analytics}>analytics</div>
 				<div className={styles.game__content}>
-					<div className={styles.content__header}>header</div>
+					<div className={styles.content__header}>
+						<div className={styles.header__avatar}>
+							<img src={ava} alt="avatar"/>
+						</div>
+						<div className={styles.header__info}>
+							<h3 className={styles.header__title}>your favourite decks</h3>
+							<div className={styles.header__decks}>
+								<div className={`${styles.decks__item} ${styles.decks__item_6}`}>
+									<div className={styles.tooltip}>
+										<div className={styles.tooltip_wrap}>
+											<h4 className={styles.tooltip_title}>Task title </h4>
+										</div>
+									</div>
+								</div>
+								<div className={`${styles.decks__item} ${styles.decks__item_2}`}>
+									<div className={styles.tooltip}>
+										<div className={styles.tooltip_wrap}>
+											<h4 className={styles.tooltip_title}>Task title </h4>
+										</div>
+									</div>
+								</div>
+								<div className={`${styles.decks__item} ${styles.decks__item_3}`}>
+									<div className={styles.tooltip}>
+										<div className={styles.tooltip_wrap}>
+											<h4 className={styles.tooltip_title}>Task title </h4>
+										</div>
+									</div>
+								</div>
+								<div className={`${styles.decks__item} ${styles.decks__item_4}`}>
+									<div className={styles.tooltip}>
+										<div className={styles.tooltip_wrap}>
+											<h4 className={styles.tooltip_title}>Task title </h4>
+										</div>
+									</div>
+								</div>
+								<div className={`${styles.decks__item} ${styles.decks__item_5}`}>
+									<div className={styles.tooltip}>
+										<div className={styles.tooltip_wrap}>
+											<h4 className={styles.tooltip_title}>Task title </h4>
+										</div>
+									</div>
+								</div>
+								<div className={`${styles.decks__item} ${styles.decks__item_1}`}>
+									<div className={styles.tooltip}>
+										<div className={styles.tooltip_wrap}>
+											<h4 className={styles.tooltip_title}>Task title </h4>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 					<div className={styles.content__main}>
 						<div className={styles.main__card}>card</div>
 						<div className={styles.main__deck}>
-							<Deck_blue/>
-							<Deck_red/>
-							<Deck_green/>
-							<Deck_yellow/>
+							<Deck_blue2/>
+							{/*<Deck_green/>*/}
+							{/*<Deck_red/>*/}
+							{/*<Deck_yellow/>*/}
+							{/*<Deck_green2/>*/}
+							{/*<Deck_blue/>*/}
 						</div>
 					</div>
 					<div className={styles.content__buttons}>buttons</div>
