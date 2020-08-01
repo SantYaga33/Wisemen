e210e3349008a9bb8843c19bb09e519cffea3297
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-08-01 21:46:14 +0400

    123

--- src/main/ui/components/game/analytics/graph/Graph.jsx
+++ src/main/ui/components/game/analytics/graph/Graph.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styles  from './Graph.module.css';
 import graphImg  from '../../../../images/graph.png';
 import Settings from "../settings/Settings";
-import dragon from '../../../../video/dragon.mp4'
+
 
 
 const Graph = ({setCardFace}) => {
@@ -23,11 +23,6 @@ const Graph = ({setCardFace}) => {
 		<div className={classForGraph}>
 			<div className={styles.graph__title}>Graph</div>
 			<div className={styles.graph__graph}>
-
-			<video className={styles.graph__video} id='dragon' controls autoPlay muted={true}>
-				<source src={dragon} type="video/mp4"/>
-			</video>
-
 
 				{/*<img src={graphImg} alt="graph"/>*/}
 			</div>
